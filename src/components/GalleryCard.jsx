58b2d0242e75a756471a7209e6bc6e528e8c1ee9
samantha-marsee <sam572@cornell.edu>
2025-02-12 15:27:58 +0000

    ternary and && operators

--- src/components/GalleryCard.jsx
+++ src/components/GalleryCard.jsx
@@ -1,6 +1,6 @@
 import Citation from "./Citation";
 
-export default function GalleryCard({ imgUri, altText, citation, caption, color = '#e2e8f0' }) {
+export default function GalleryCard({ imgUri, altText, citation = undefined, caption = undefined, color = '#e2e8f0' }) {
   return (
     <div
       className="card"
@@ -9,10 +9,14 @@ export default function GalleryCard({ imgUri, altText, citation, caption, color 
       <img src={imgUri} alt={altText} />
 
       <div className="caption">
+        {!!caption &&
         <p style={{ borderBottom: "1px solid #000000", paddingBottom: "2px" }}>
           {caption}
         </p>
+        }
+        {citation ?
         <Citation citation={citation} align="center" />
+        : undefined}
       </div>
     </div>
   )
